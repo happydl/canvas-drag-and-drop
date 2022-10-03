c29e8b080badc8e4b4b217a991412b4d0fddf2e0
ldm <liudamingdl@gmail.com>
2022-10-03 00:53:04 -0400

    draw static columns and tasks

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 // init data
 const config = {
-    volumeWidth: 100,
-    volumeGapWidth: 10,
+    columnWidth: 120,
+    columnGapWidth: 10,
+    columnMinHeight:50,
     taskWidth: 80,
     taskHeight: 40,
+    taskGapHeight:10,
     data: {
-        volumes: [
+        columns: [
             {
                 name: "V1",
                 tasks: ["ABC", "DEF", "GHI"]
             },
             {
                 name: "V2",
-                tasks: ["Dog", "Cat", "Pig"]
+                tasks: ["Dog", "Cat", "Pig", "Tree", "Flower"]
             },
             {
                 name: "V3",
-                tasks: ["Building", "Garden", "Tree"]
+                tasks: ["Building", "Garden"]
             }
         ]
-    }
+    },
+    columnBackGroundColor: '#FD0',
+    taskBackGroundColor: 'white'
 }
 
 
@@ -42,7 +46,7 @@ class Shape {
 
 
 
-/** task in volume */
+/** task in column */
 class Task extends Shape {
 
     constructor(title, tag) {
@@ -52,11 +56,15 @@ class Task extends Shape {
     }
 
     draw() {
-
+        const ctx = document.getElementById('canvas').getContext('2d');
+        ctx.save()
+        ctx.fillStyle = config.taskBackGroundColor;
+        ctx.fillRect(this.transform.x, this.transform.y, config.columnWidth - config.taskGapHeight * 2, config.taskHeight);
+        ctx.restore();
     }
 }
 
-class Volume extends Shape {
+class Column extends Shape {
     constructor() {
         super();
         this.taskList = [];
@@ -64,6 +72,33 @@ class Volume extends Shape {
 
     draw() {
 
+        const ctx = document.getElementById('canvas').getContext('2d');
+        ctx.save()
+
+        ctx.fillStyle = config.columnBackGroundColor;
+
+        // calculate height of this column
+        let volHeight = this.taskList.length * config.taskHeight;
+
+        // # of gaps is # of tasks - 1
+        if (this.taskList.length > 0) {
+            volHeight += (this.taskList.length + 1) * config.taskGapHeight;
+        }
+
+        // set height to min height if the column is empty
+        volHeight = Math.max(volHeight, config.columnMinHeight);
+
+        ctx.fillRect(this.transform.x, this.transform.y, config.columnWidth, volHeight);
+
+        // set transform for each task in this column
+        for (let i=0; i<this.taskList.length; i++) {
+            let xTask = this.transform.x + config.taskGapHeight;
+            this.taskList[i].transform.x = xTask;
+            this.taskList[i].transform.y = i * (config.taskHeight + config.taskGapHeight) + config.taskGapHeight;
+            this.taskList[i].draw();
+        }
+
+        ctx.restore();
     }
 }
 
@@ -84,7 +119,7 @@ class GameController {
     }
 
     constructor(config) {
-        this.volumeList = [];
+        this.columnList = []; // column list
         if (config) {
             this.config = config;
             if (config.data) {
@@ -99,19 +134,14 @@ class GameController {
      */
     loadData(data) {
 
-        // let v1 = new Volume();
-        // v1.taskList.push(new Task());
-
-        // data.forEach(vol => {
-        //     let v = new Volume();
-        //     vol.forEach(task => {
-
-        //     });
-        // });
-
-        // for (vol in data) {
-
-        // }
+        data.columns.forEach(vol => {
+            let v = new Column(vol.name);
+            vol.tasks.forEach(taskName => {
+                let t = new Task(taskName, "tagA");
+                v.taskList.push(t);
+            });
+            this.columnList.push(v);
+        });
 
     }
 
@@ -122,8 +152,22 @@ class GameController {
     render() {
 
         const ctx = document.getElementById('canvas').getContext('2d');
-        ctx.fillStyle = '#FD0';
-        ctx.fillRect(0, 0, 75, 75);
+        
+
+        // draw columns background
+        for (let i=0; i<this.columnList.length; i++) {
+            let xStart = i * this.config.columnGapWidth + i * this.config.columnWidth;
+            let yStart = 0;
+            this.columnList[i].transform.x = xStart;
+            this.columnList[i].transform.y = yStart;
+            this.columnList[i].draw();
+        }
+
+        // draw tasks
+        // for ()
+
+
+
 
     }
 }
